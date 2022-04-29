2175e7db16811afd7fe36408d8329cee173cb0b1
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-MacBook-Air.local>
2022-04-29 17:27:52 +0100

    Started footer section

--- pages/index.tsx
+++ pages/index.tsx
@@ -147,9 +147,7 @@ const Home: NextPage = () => {
         detectRetina: true,
       }}
     />
-    <div className="text-center bg-white h-10 mt-8 dark-blue">
-      <p className="mx-auto mt-8 items-center justify-center text-white text-[13px]"> Copyright © 2022 | Adetunji Adeyinka </p>
-    </div>
+    
     </>
   )
 };
