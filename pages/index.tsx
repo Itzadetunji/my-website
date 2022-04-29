777277af612325b643222845328d7335548714fc
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-MacBook-Air.local>
2022-04-29 17:27:27 +0100

    Started footer section

--- pages/index.tsx
+++ pages/index.tsx
@@ -148,7 +148,7 @@ const Home: NextPage = () => {
       }}
     />
     <div className="text-center bg-white h-10 mt-8 dark-blue">
-      <p className="mx-auto mt-8 items-center justify-center text-white text-[20px]"> Copyright © 2022 | Adetunji Adeyinka </p>
+      <p className="mx-auto mt-8 items-center justify-center text-white text-[13px]"> Copyright © 2022 | Adetunji Adeyinka </p>
     </div>
     </>
   )
