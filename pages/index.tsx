3b631f65d7322a52cbd36a47b7c7e6b9f18dea10
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-MacBook-Air.local>
2022-04-29 17:29:54 +0100

    Edited hr

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,7 +28,7 @@ const Home: NextPage = () => {
         <h1 className="text-white ml-9 font-semibold text-4xl">I'm</h1>
         <br/>
         <h1 className="text-white ml-9 font-semibold leading-10 -mt-4 text-4xl">Adetunji <br/>Adeyinka</h1>
-        <hr className="light-blue w-[150px] h-1 ml-[35px] border-transparent mt-2"/>
+        <hr className="light-blue w-[150px] h-1 ml-[35px] border-transparent my-4"/>
         <div className="w-64 h-auto mx-auto mt-8 items-center justify-center">
           <p className="text-white">I’m a fullstack web developer who provides quality services of industry satandard Dire was here.</p>
           <img src="/icons/learn-more.svg" alt="" className="w-auto h-5 mt-2"/>
