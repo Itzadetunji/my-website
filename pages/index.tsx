95f948be37632c7b9578827bc71a81c6f7ecce9d
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-MacBook-Air.local>
2022-04-29 17:11:46 +0100

    Modified Particles.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,8 +26,8 @@ const Home: NextPage = () => {
       <div className="mt-16 z-50">
         <h1 className="text-white ml-9 font-semibold text-4xl">I'm</h1>
         <br/>
-        <h1 className="text-white ml-9 font-semibold leading-8 -mt-4 text-4xl">Adetunji <br/>Adeyinka</h1>
-        <hr className="light-blue w-[95px] h-1 ml-[35px] border-transparent mt-2"/>
+        <h1 className="text-white ml-9 font-semibold leading-10 -mt-4 text-4xl">Adetunji <br/>Adeyinka</h1>
+        <hr className="light-blue w-[150px] h-1 ml-[35px] border-transparent mt-2"/>
         <div className="w-64 h-auto mx-auto mt-8 items-center justify-center">
           <p className="text-white">I’m a fullstack web developer who provides quality services of industry satandard Dire was here.</p>
           <img src="/icons/learn-more.svg" alt="" className="w-auto h-5 mt-2"/>
