71f9a753d1f62dc766fee9b0d4e1baf6df283c6f
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-MacBook-Air.local>
2022-04-29 17:28:16 +0100

    Created footer component

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className="text-center bg-white h-10 mt-8 dark-blue">
+      <p className="mx-auto mt-8 items-center justify-center text-white text-[13px]"> Copyright © 2022 | Adetunji Adeyinka </p>
+    </div>
   )
 }
 
