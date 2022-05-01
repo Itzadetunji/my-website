1ffbd9a10a0638153643d83f360b0ccbcbbc4404
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-MacBook-Air.local>
2022-05-01 14:15:00 +0100

    Created particle component for particles

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,24 +5,13 @@ import { useEffect } from 'react'
 import styles from '../styles/Home.module.css'
 import Footer from './components/Footer';
 import Navbar from './components/Navbar'
+import ParticleComponent from './components/ParticleComponent'
 
 const Home: NextPage = () => {
-  const particlesInit = async (main) => {
-    console.log(main);
-
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    await loadFull(main);
-  };
-
-  const particlesLoaded = (container) => {
-    console.log(container);
-  };
   return (
     <>
       <Navbar/>
-      <div className="mt-16" style={{zIndex: -2}}>
+      <div className="mt-16">
         <h1 className="text-white ml-9 font-semibold text-4xl">I'm</h1>
         <br/>
         <h1 className="text-white ml-9 font-semibold leading-10 -mt-4 text-4xl">Adetunji <br/>Adeyinka</h1>
@@ -40,113 +29,8 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
-        <Particles
-      id="tsparticles"
-      init={particlesInit}
-      loaded={particlesLoaded}
-      options={{
-        fullScreen: {enable: true, zIndex: 0},
-        fpsLimit: 120,
-        interactivity: {
-          events: {
-            onClick: {
-              enable: true,
-              mode: "grab",
-            },
-            onHover: {
-              enable: true,
-              mode: "repulse",
-            },
-            resize: true,
-          },
-          modes: {
-            grab: {
-              distance: 85.26326046708033,
-              line_linked: {
-                opacity: 0.4442177654201152
-              }
-            },
-            bubble: {
-              distance: 250,
-              size: 0,
-              duration: 2,
-              opacity: 0,
-            },
-            repulse: {
-              distance: 64.96248416539453,
-              duration: 0.4
-            },
-            push: {
-              particles_nb: 4
-            },
-            remove: {
-              particles_nb: 2
-            }
-          },
-        },
-        particles: {
-          color: {
-            value: "#ffffff",
-          },
-          links: {
-            color: "#ffffff",
-            distance: 20,
-            enable: true,
-            opacity: 1,
-            width: 1,
-          },
-          collisions: {
-            enable: false,
-          },
-          move: {
-            direction: "none",
-            enable: true,
-            outModes: {
-              default: "bounce",
-            },
-            random: false,
-            speed: 1.5,
-            straight: false,
-          },
-          number: {
-            density: {
-              enable: true,
-              area: 800,
-            },
-            value: 500,
-          },
-          opacity: {
-            value: 0.1,
-            random: true,
-            anim: {
-              enable: true,
-              speed: 1,
-              opacity_min: 0,
-              sync: false
-            }
-          },
-          shape: {
-            type: "circle",
-            stroke: {
-              width: 0,
-              color: "#000000",
-            }
-          },
-          size: {
-            value: 1,
-            random: true,
-            anim: {
-              enable: false,
-              speed: 1,
-              size_min: 0.3,
-              sync: false
-            }
-          },
-        },
-        detectRetina: true,
-      }}
-    />
-    <Footer/>
+      <ParticleComponent/>
+      <Footer/>
     </>
   )
 };
