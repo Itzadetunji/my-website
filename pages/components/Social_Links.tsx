1634b8c6008dfe8b4e47efa77d9024653e13f378
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-MacBook-Air.local>
2022-05-01 15:25:42 +0100

    Created Sociallinks component

---
+++ pages/components/Social_Links.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Social_Links() {
+  return (
+    <div className="flex content-evenly flex-wrap mt-5">
+      <div className="mx-auto">
+        <img src="/icons/facebook.svg" alt="" className="w-8 h-8 mx-auto inline"/>
+        <img src="/icons/instagram.svg" alt="" className="w-8 h-8 inline mx-3"/>
+        <img src="/icons/twitter.svg" alt="" className="w-8 h-8 mx-auto inline"/>
+      </div>
+    </div>
+  )
+}
+
+export default Social_Links
