e02abe7174471a6bc31e8937f2622fc2339ad6b6
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-MacBook-Air.local>
2022-05-01 14:24:25 +0100

    Created about component

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,6 +7,10 @@ function about() {
   return (
     <>
       <Navbar/>
+      <div className='mt-16'>
+        <h1 className="text-white ml-9 font-semibold leading-10 -mt-4 text-4xl">Adetunji <br/>Adeyinka</h1>
+        <hr className="light-blue w-[150px] h-1 ml-[35px] border-transparent my-4"/>
+      </div>
       <div className="w-64 h-auto mx-auto mt-8 items-center justify-center">
         <img src="/images/me.jpeg" className="mt-5 rounded-full"/>
       </div>
