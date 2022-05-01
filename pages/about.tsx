974cc1795dffac752104e0f9a69679b7bb481619
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-MacBook-Air.local>
2022-05-01 14:21:50 +0100

    Created about component

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Footer from './components/Footer'
 import Navbar from './components/Navbar'
+import ParticleComponent from './components/ParticleComponent'
 
 function about() {
   return (
     <>
       <Navbar/>
+      <ParticleComponent/>
+      <Footer/>
     </>
   )
 }
--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div className="text-center bg-white h-10 mt-8 dark-blue">
-      <p className="mx-auto mt-8 items-center justify-center text-white text-[13px]"> Copyright © 2022 | Adetunji Adeyinka </p>
+      <p className="mx-auto mt-8 items-center justify-center text-white text-[13px] right-0"> Copyright © 2022 | Adetunji Adeyinka </p>
     </div>
   )
 }
