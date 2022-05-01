8550cf0f16dca17c6e1d611f060ef0000dc42d96
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-MacBook-Air.local>
2022-05-01 14:10:44 +0100

    Changed zIndex of particled

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ const Home: NextPage = () => {
   return (
     <>
       <Navbar/>
-      <div className="mt-16 z-50">
+      <div className="mt-16" style={{zIndex: -2}}>
         <h1 className="text-white ml-9 font-semibold text-4xl">I'm</h1>
         <br/>
         <h1 className="text-white ml-9 font-semibold leading-10 -mt-4 text-4xl">Adetunji <br/>Adeyinka</h1>
