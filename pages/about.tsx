869bea83a83a36796bc3fc231987a036524ba8a2
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-07-12 18:52:04 +0100

    Chore: Changes to about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,8 +7,23 @@ const about = () => {
   return (
     <>
       <Navbar/>
-      
-      <ParticleComponent/>
+      <div className="mt-[165px] mx-auto justify-center">
+        <div className='mx-[35px]'>
+          <div>
+            <p className='text-white font-[600] text-[55px]'>I'm</p>
+            <p className='text-white font-[600] text-[55px] leading-[60px]'>Adetunji Adeyinka</p>
+            <hr className='light-blue border-[3px] w-[224px] md:w-[204px]  mt-[35px]'/>
+          </div>
+          <div className='text-white mt-[73px]'>
+            <img src="./images/me.jpeg" alt="" className='w-[363px] h-[528px]'/>
+            <div className='mt-[100px] text-[25px] text-center'>
+              <p>I’m a fullstack web developer who provides quality services of industry satandard.</p>
+              <p className='mt-[20px]'>I love finding new ways to solve existing problems ;)</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <ParticleComponent/> */}
       <Footer/>
     </>
   )
