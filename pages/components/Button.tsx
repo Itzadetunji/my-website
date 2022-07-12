0baee0127e095aad4913ff3783b947bf48e2dc4c
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-07-12 16:43:01 +0100

    Chore: Changes to button.tsx

--- pages/components/Button.tsx
+++ pages/components/Button.tsx
@@ -5,8 +5,8 @@ type Props = {}
 
 const Button: NextPage = () => {
   return (
-    <div className='mx-auto text-center'>
-      <button className=''>Know More -></button>
+    <div className='mx-[84px] rounded-full text-center items-center justify-center grey w-[260px] h-[94px] button'>
+      <p className='pt-[28px] text-[25px] text-white'>Know More -></p>
     </div>
   )
 }
