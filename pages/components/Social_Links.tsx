c17d29473ef24164c5401d21eed0f00d69051445
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-07-12 16:28:14 +0100

    Chore: . Changes to social_links.tsx

--- pages/components/Social_Links.tsx
+++ pages/components/Social_Links.tsx
@@ -3,12 +3,14 @@ import React from 'react'
 
 const Social_Links: NextPage = () => {
   return (
-    <div className="flex content-evenly flex-wrap mt-5">
-      <div className="mx-auto">
-        <img src="/icons/facebook.svg" alt="" className="w-8 h-8 mx-auto inline"/>
-        <img src="/icons/instagram.svg" alt="" className="w-8 h-8 inline mx-3"/>
-        <img src="/icons/twitter.svg" alt="" className="w-8 h-8 mx-auto inline"/>
+    <div className="flex mt-5 space-x-[30px] mx-[30px]">
+      <hr className='border w-[70px] my-[15px]'/>
+      <div className="mx-auto flex space-x-[40px]">
+        <img src="/icons/facebook.svg" alt=""/>
+        <img src="/icons/instagram.svg" alt=""/>
+        <img src="/icons/twitter.svg" alt=""/>
       </div>
+      <hr className='border w-[70px] my-[15px]'/>
     </div>
   )
 }
