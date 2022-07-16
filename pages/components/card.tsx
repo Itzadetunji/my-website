6531fd84868abeff0d6ef001d67e711d3613aa76
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-07-16 15:24:21 +0100

    Chore: Changes to card.tsx

--- pages/components/card.tsx
+++ pages/components/card.tsx
@@ -9,9 +9,9 @@ interface cardProps {
 
 const Card: React.FC<cardProps> = ({icon, text, title}) => {
   return (
-    <div className='grey w-full rounded-[30px]'>
+    <div className='grey w-[334px] rounded-[30px] mx-auto'>
       <div className='flex flex-col mt-[79px]'>
-        <img src='./icons/bullseye.svg' alt="" className='w-[80px] h-[80px] items-center justify-center mx-auto mt-[47px]'/>
+        <img src='./icons/bullseye.svg' alt="" className='w-[90px] h-[90px] items-center justify-center mx-auto mt-[47px]'/>
         <p className='text-center text-white pt-[35px] font-bold light-blue-text text-[23px]'>Goal Oriented</p>
         <p className='text-center text-white mx-[27px] pt-[35px] pb-[37px] text-[17px]'>I check your website before making changes. I seek for potential user problems, big or small, and offer fixes to ensure your user is on the right path to attain their goals.</p>
       </div>
