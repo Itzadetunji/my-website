6e3b41bb72113e3508406cfb5f0064b0e9e6d4c7
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-07-16 17:48:52 +0100

    Chore: Completed workprocess for mobile ✅

--- pages/components/WorkProcess.tsx
+++ pages/components/WorkProcess.tsx
@@ -3,22 +3,24 @@ import React from 'react'
 
 interface cardProps {
   iconpath: string,
+  width: string,
+  height: string,
   title: string,
   description: string
 }
 
 const servicecontent = 1;
 
-const ServiceCard: React.FC<cardProps> = ({iconpath, title, description}) => {
+const WorkProcess: React.FC<cardProps> = ({iconpath, width, height, title, description}) => {
   return (
-    <div className='grey w-[260px] rounded-[30px] mx-auto drop-shadow duration-500 ease-in delay-[10ms]'>
-      <div className='flex flex-col mt-[79px]'>
-        <img src={iconpath} alt="" className='w-[90px] h-[90px] items-center justify-center mx-auto mt-[47px]'/>
-        <p className='text-center text-white pt-[35px] font-bold light-blue-text text-[23px]'>{title}</p>
-        <p className='text-center text-white mx-[27px] pt-[35px] pb-[37px] text-[17px] leading-[35px]'>{description}</p>
+    <div className='grey w-[290px] h-[260px] mt-[79px] rounded-[67px] mx-auto drop-shadow duration-500 ease-in delay-[10ms]'>
+      <div className='flex flex-col py-[10px]'>
+        <img src={iconpath} alt="" className={`w-[${width}] h-[${height}] items-center justify-center mx-auto mt-[23px]`}/>
+        <p className='text-center text-white pt-[7px] font-bold light-blue-text text-[23px]'>{title}</p>
+        <p className='text-center text-white mx-[22px] pt-[7px] pb-[37px] text-[12.4px] leading-[27px]'>{description}</p>
       </div>
     </div>
   )
 }
 
-export default ServiceCard
+export default WorkProcess
