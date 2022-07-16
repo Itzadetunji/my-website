6a4bd2757a6dd7154e0911e5392811d9d8e011fc
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-07-16 15:26:40 +0100

    Chore: Changes to card.tsx

--- pages/components/card.tsx
+++ pages/components/card.tsx
@@ -9,7 +9,7 @@ interface cardProps {
 
 const Card: React.FC<cardProps> = ({icon, text, title}) => {
   return (
-    <div className='grey w-[334px] rounded-[30px] mx-auto'>
+    <div className='grey w-[334px] rounded-[30px] mx-auto drop-shadow duration-500 ease-in delay-[10ms]'>
       <div className='flex flex-col mt-[79px]'>
         <img src='./icons/bullseye.svg' alt="" className='w-[90px] h-[90px] items-center justify-center mx-auto mt-[47px]'/>
         <p className='text-center text-white pt-[35px] font-bold light-blue-text text-[23px]'>Goal Oriented</p>
