ee2b92bf3ca82eb9a3b483341c0a8084288f3527
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-07-16 16:31:04 +0100

    Chore: Created workprocesses.tsx

---
+++ pages/components/WorkProcess.tsx
@@ -0,0 +1,24 @@
+import { NextPage } from 'next'
+import React from 'react'
+
+interface cardProps {
+  iconpath: string,
+  title: string,
+  description: string
+}
+
+const servicecontent = 1;
+
+const ServiceCard: React.FC<cardProps> = ({iconpath, title, description}) => {
+  return (
+    <div className='grey w-[260px] rounded-[30px] mx-auto drop-shadow duration-500 ease-in delay-[10ms]'>
+      <div className='flex flex-col mt-[79px]'>
+        <img src={iconpath} alt="" className='w-[90px] h-[90px] items-center justify-center mx-auto mt-[47px]'/>
+        <p className='text-center text-white pt-[35px] font-bold light-blue-text text-[23px]'>{title}</p>
+        <p className='text-center text-white mx-[27px] pt-[35px] pb-[37px] text-[17px] leading-[35px]'>{description}</p>
+      </div>
+    </div>
+  )
+}
+
+export default ServiceCard
