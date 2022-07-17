d83fcfe6f9a68818befcf9269df2636a6df86774
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-07-17 15:16:52 +0100

    Chore: Added portfolio.tsx

---
+++ pages/portfolio.tsx
@@ -0,0 +1,42 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import { useEffect } from 'react'
+import styles from '../styles/Home.module.css'
+import Footer from './components/Footer';
+import Navbar from './components/Navbar'
+import ParticleComponent from './components/ParticleComponent'
+import Social_Links from './components/SocialLinks'
+import Button from './components/Button'
+
+const index : NextPage = () => {
+  return (
+    <>
+      <Navbar/>
+      <div className="mt-[165px] mx-auto justify-center">
+        <div className='mx-[35px]'>
+        <div>
+            <p className='text-white font-[600] text-[40px]'>My Portfolio</p>
+            <hr className='light-blue border-[3px] w-[55px] mt-[16px]'/>
+          </div>
+          <div className='text-white mt-[73px]'>
+            <img src="./images/me.jpeg" alt="" className='w-[363px] h-[528px]'/>
+            <div className='mt-[100px] text-[25px] text-center'>
+              <p>I’m a fullstack web developer who provides quality services of industry satandard.</p>
+              <p className='mt-[20px]'>I love finding new ways to solve existing problems ;)</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <ParticleComponent/> */}
+      <div className='mt-[100px]'>
+        <Button link={'#'} text={'Know More'} height={'260px'} width={'94px'}/>
+      </div>
+      <div className='mt-[95px]'>
+        <Social_Links/>
+      </div>
+      <Footer/>
+    </>
+  )
+};
+export default index
