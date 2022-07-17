063bdfd7377c43426b0934645970756555bd714f
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-07-17 14:39:47 +0100

    Chore: Done with mobile view of tech stack component ✅

--- pages/components/TechStack.tsx
+++ pages/components/TechStack.tsx
@@ -38,9 +38,81 @@ const TechStack: NextPage = () => {
     {
       path: "../icons/stack-icons/Nodejs.svg",
       name: "Javascript"
-    }
+    },
+    {
+      path: "../icons/stack-icons/HTML5.svg",
+      name: "HTML5"
+    },
+    {
+      path: "../icons/stack-icons/CSS3.svg",
+      name: "CSS3"
+    },
+    {
+      path: "../icons/stack-icons/Javascript.svg",
+      name: "Javascript"
+    },
+    {
+      path: "../icons/stack-icons/React.svg",
+      name: "Reactjs"
+    },
+    {
+      path: "../icons/stack-icons/Nextjs.svg",
+      name: "CSS3"
+    },
+    {
+      path: "../icons/stack-icons/Vue.svg",
+      name: "Javascript"
+    },
+    {
+      path: "../icons/stack-icons/NPM.svg",
+      name: "HTML5"
+    },
+    {
+      path: "../icons/stack-icons/yarn.svg",
+      name: "CSS3"
+    },
+    {
+      path: "../icons/stack-icons/Nodejs.svg",
+      name: "Javascript"
+    },
   ]
   const TechStackContents2 =[
+    {
+      path: "../icons/stack-icons/Typescript.svg",
+      name: "HTML5"
+    },
+    {
+      path: "../icons/stack-icons/mysql.svg",
+      name: "CSS3"
+    },
+    {
+      path: "../icons/stack-icons/mongodb.svg",
+      name: "Javascript"
+    },
+    {
+      path: "../icons/stack-icons/Webpack.svg",
+      name: "HTML5"
+    },
+    {
+      path: "../icons/stack-icons/Tailwindcss.svg",
+      name: "CSS3"
+    },
+    {
+      path: "../icons/stack-icons/Bootstrap.svg",
+      name: "Javascript"
+    },
+    {
+      path: "../icons/stack-icons/Github.svg",
+      name: "HTML5"
+    },
+    {
+      path: "../icons/stack-icons/VSCode.svg",
+      name: "CSS3"
+    },
+    {
+      path: "../icons/stack-icons/Postman.svg",
+      name: "Javascript"
+    },
     {
       path: "../icons/stack-icons/Typescript.svg",
       name: "HTML5"
