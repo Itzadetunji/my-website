0e1c4b1766081dc613276615a029890874f5bbe0
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-07-18 12:03:35 +0100

    Chore: Removed nodemailer

---
+++ pages/api/contact.ts
@@ -0,0 +1,49 @@
+// Get Contact Form Data and send email with nodemailer
+const nodemailer = require("nodemailer");
+
+export default async function handler(req, res) {
+	const { name, email, subject, message } = req.body;
+
+	if (!name || !email || !subject || !message) {
+		return res.status(400).json({ error: "All fields are required" });
+	}
+
+	// Create transporter
+	const transporter = nodemailer.createTransport({
+		service: "gmail",
+		auth: {
+			user: "eosabiya@gmail.com",
+			pass: process.env.EMAIL_PASSWORD,
+		},
+	});
+
+	// Create mail options
+	const mailOptions = {
+		from: email,
+		to: "eosabiya@gmail.com",
+		replyTo: email,
+		subject: `${subject} — Contact Form: eniolaosabiya.com`,
+		text: message,
+		html: `
+			<h1>New message from ${email}</h1>
+			<h2>Name:</h2>
+			<h3>${name}</h3>
+			<h2>Subject:</h2>
+			<h3>${subject}</h3>
+			<h2>Message:</h2>
+			<h3>${message}</h3>
+		`,
+	};
+
+	try {
+		// Send email
+		await transporter.sendMail(mailOptions, (err, info) => {
+			if (err) {
+				return res.status(500).json({ error: err.message });
+			}
+			return res.status(200).json({ message: "Email sent" });
+		});
+	} catch (error) {
+		return res.status(500).json({ error: error.message });
+	}
+}
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
 import Footer from './components/Footer';
 import Navbar from './components/Navbar'
