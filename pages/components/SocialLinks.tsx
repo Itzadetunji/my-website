7071de93d310140acd9003f6cbc3127f3ec4d553
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-07-18 12:54:31 +0100

    Chore: Updated sociallinks.tsx

--- pages/components/SocialLinks.tsx
+++ pages/components/SocialLinks.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from 'next'
+import Link from 'next/link'
 import React from 'react'
 
 const Social_Links: NextPage = () => {
@@ -6,9 +7,21 @@ const Social_Links: NextPage = () => {
     <div className="flex mt-5 space-x-[30px] mx-auto justify-center">
       <hr className='border w-[70px] my-[15px]'/>
       <div className="mx-auto flex space-x-[40px]">
-        <img src="/icons/facebook.svg" alt=""/>
-        <img src="/icons/instagram.svg" alt=""/>
-        <img src="/icons/twitter.svg" alt=""/>
+        <Link href="https://www.facebook.com/adetunji.adeyinka.1238" passHref>
+          <a target="_blank" rel="noopener noreferrer">
+            <img src="/icons/facebook.svg" alt=""/>
+          </a>
+        </Link>
+        <Link href="https://www.instagram.com/itzadetunji/" passHref>
+          <a target="_blank" rel="noopener noreferrer">
+            <img src="/icons/instagram.svg" alt=""/>
+          </a>
+        </Link>
+        <Link href="https://twitter.com/itzadetunji1" passHref>
+          <a target="_blank" rel="noopener noreferrer">
+            <img src="/icons/twitter.svg" alt=""/>
+          </a>
+        </Link>
       </div>
       <hr className='border w-[70px] my-[15px]'/>
     </div>
