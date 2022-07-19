12edc4c175c0ac711e55f0b708e551bfdcafb570
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-07-19 15:26:24 +0100

    Chore: Completed desktop view for index.tsx ✅

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,25 +22,29 @@ const index : NextPage = () => {
               <p className='text-white font-[600] text-[55px] leading-[60px]'>Adetunji Adeyinka</p>
               <hr className='light-blue border-[3px] w-[224px] sm:w-[204px] mt-[35px]'/>
             </div>
-            <div className='text-white mt-[73px] xl:-mt-[20px]  lg:flex lg:flex-row lg:space-x-[100px] xl:space-x-[170px]'>
+            <div className='text-white mt-[73px] xl:-mt-[20px] lg:flex lg:flex-row md:items-end lg:space-x-[100px] xl:space-x-[170px] 2xl:space-x-[280px]'>
               <img src="./images/me.jpeg" alt="" className='w-[363px] xl:w-[400px] h-[528px] xl:h-[582px] mx-auto lg:mx-[0px]'/>
-              <div className='mt-[100px] lg:mt-[0] text-center lg:text-left'>
-                <p className='text-white font-[600] text-[55px] lg:text-[40px] hidden lg:block'>I'm</p>
-                <p className='text-white font-[600] text-[55px] lg:text-[40px] leading-[60px] hidden lg:block'>Adetunji Adeyinka</p>
-                <hr className='light-blue border-[3px] w-[224px] sm:w-[204px] lg:w-[150px] mt-[35px] lg:mt-[20px] hidden lg:block'/>
-                <div className='w-[300px] xl:w-[360px] font-light text-[25px] lg:text-[16px] xl:text-[20px] xl:leading-[30px] '>
+              <div className='mt-[100px] lg:mt-[0px] text-center lg:text-left'>
+                <p className='text-white font-[600] text-[55px] lg:text-[35px] hidden lg:block'>I'm</p>
+                <p className='text-white font-[600] text-[55px] lg:text-[37px] leading-[60px] hidden lg:block'>Adetunji Adeyinka</p>
+                <hr className='light-blue border-[3px] w-[224px] sm:w-[204px] lg:w-[140px] mt-[35px] lg:mt-[20px] hidden lg:block'/>
+                <div className='lg:w-[360px] xl:w-[450px] text-center lg:text-left mx-auto font-light text-[25px] lg:text-[16px] xl:text-[20px] xl:leading-[30px] 2xl:leading-[33px]'>
                   <p className='mt-[20px] lg:mt-[40px]'>
                   I am a fullstack web developer based in Lagos State, Nigeria. I started my coding journey in 2017 and now I have over 4+ years of experience. 
                   </p>
-                  <p className='mt-[20px] xl:mt-[0px]'>I love finding new ways to solve existing problems ;)</p>
-                  <p className='mt-[20px] xl:mt-[0px]'>I also have this excessive hunger to continue learning new things everyday 😅 </p>
-                  <div className='w-fit px-[20px] py-[13px] grey rounded-full flex space-x-3 hovering-arrow mt-[30px]'>
-                    <p className='text-[16px]'>Know more</p>
-                    <img src="./icons/Arrow.svg" alt="" className='arrow'/>
+                  <p className='mt-[20px]'>I love finding new ways to solve existing problems ;)I love finding new ways to solve existing problems ;)</p>
+                  <p className='mt-[20px]'>I also have this excessive hunger to continue learning new things everyday 😅I also have this excessive hunger to continue learning new things everyday 😅 </p>
+                  <div className='hidden lg:block'>
+                    <div className='w-fit px-[20px] py-[13px] grey rounded-full flex space-x-3 hovering-arrow mt-[30px]'>
+                      <p className='text-[16px]'>Know more</p>
+                      <img src="./icons/Arrow.svg" alt="" className='arrow'/>
+                    </div>
                   </div>
                 </div>
               </div>
-              <div className='hidden lg:flex lg:flex-col mt-5 lg:space-y-[30px] xl:space-y-[20px] xl:mt-[80px]'>
+            </div>
+            <div className='lg:absolute lg:top-[200px] xl:top-[150px] right-[50px]'>
+              <div className='hidden lg:flex lg:flex-col mt-5 lg:space-y-[30px] xl:mt-[80px]'>
                 <div className='border-l-white border-l-[1px] h-[70px] lg:ml-[30px]'/>
                 <div className="mx-auto flex flex-col space-y-[40px]">
                   <div className='w-14 h-14 flex hoverAnimation items-center justify-center'>
@@ -68,6 +72,7 @@ const index : NextPage = () => {
                 <div className='border-l-white border-l-[1px] h-[70px] lg:ml-[30px]'/>
               </div>
             </div>
+            
           </div>
         </div>
         <div className='mt-[100px] lg:hidden'>
