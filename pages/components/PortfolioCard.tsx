161aac64ddf37644df0c02cf907fe3cb55d11b03
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2022-07-26 17:09:46 +0100

    Chore: Nearly completed portfolio page

--- pages/components/PortfolioCard.tsx
+++ pages/components/PortfolioCard.tsx
@@ -10,22 +10,20 @@ interface ProfileCardProps {
 
 const PortfolioCard : React.FC<ProfileCardProps> = ({imagePath, title, description, activeLink, gitLink}) => {
   return (
-    <div className="flex flex-col space-y-[70px] pb-[40px]">
-        <div>
-          <img src={`images/portfolio/${imagePath}`} alt="" className='w-full rounded-t-[30px]'/>
-          <div className='mx-[32px] mt-[45px] text-white'>
-            <p className='text-[32px] font-[600]'>{title}</p>
-            <p className='text-[20px] font-[200] leading-[35px] mt-[5px]'>{description}</p>
-            <div className='flex flex-row space-x-[15px] mt-[25px]'>
-              {(activeLink) && <a href={activeLink}>
-                <img src="../icons/link.svg" alt="Live Preview icon" />
-              </a>}
-              {(gitLink) && <a href={gitLink}>
-                <img src="../icons/stack-icons/Github.svg" className='w-[39px] h-[39px]' alt="Github icon" />
-              </a>}
-            </div>
-          </div>
+    <div className='pb-[40px] grey rounded-[30px] mx-auto w-[500px] lg:w-[400px] xl:w-[500px]'>
+      <img src={`images/portfolio/${imagePath}`} alt="" className='w-full rounded-t-[30px]'/>
+      <div className='mx-[32px] mt-[45px] text-white'>
+        <p className='text-[32px] font-[600]'>{title}</p>
+        <p className='text-[20px] font-[200] leading-[35px] mt-[5px]'>{description}</p>
+        <div className='flex flex-row space-x-[15px] mt-[25px]'>
+          {(activeLink) && <a href={activeLink}>
+            <img src="../icons/link.svg" alt="Live Preview icon" />
+          </a>}
+          {(gitLink) && <a href={gitLink}>
+            <img src="../icons/stack-icons/Github.svg" className='w-[39px] h-[39px]' alt="Github icon" />
+          </a>}
         </div>
+      </div>
     </div>
   )
 }
--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -13,30 +13,53 @@ import PortfolioCard from './components/PortfolioCard'
 const portfolio : NextPage = () => {
   return (
     <>
-    <ParticleComponent/>
-    <div className='absolute z-50'>
-    <Navbar/>
-      <div className="mt-[165px] mx-auto justify-center">
-        <div className='mx-[35px]'>
-        <div>
-            <p className='text-white font-[600] text-[40px]'>My Portfolio</p>
-            <hr className='light-blue border-[3px] w-[55px] mt-[16px]'/>
-          </div>
-          <div className='text-white mt-[73px]'>
-            <div className='mt-[45px] text-[25px] text-center'>
-              <p>Here is a sneak peek 👀 of what I have done 🙈</p>
+    {/* <ParticleComponent/> */}
+    <div className=''>
+    {/* <Navbar/> */}
+      <div className='mx-auto'>
+        <div className="mt-[165px] mx-auto justify-center">
+          <div className='mx-[35px]'>
+          <div>
+              <p className='text-white font-[600] text-[40px]'>My Portfolio</p>
+              <hr className='light-blue border-[3px] w-[55px] mt-[16px]'/>
+            </div>
+            <div className='text-white mt-[73px]'>
+              <div className='mt-[45px] text-[25px] text-center'>
+                <p>Here is a sneak peek 👀 of what I have done 🙈</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className='mt-[100px] grey mx-[30px] rounded-[30px]'>
-        <PortfolioCard 
-          imagePath={'portfolio1.png'} 
-          title={'Service Track'} 
-          description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur iure provident mollitia ex facilis adipisci et perspiciatis error suscipit dignissimos?'} 
-          activeLink={'https://google.com'} 
-          gitLink={'https://google.com'}
-        />
+        <div className='mx-[30px] mt-[65px] grid grid-cols-1 lg:grid-cols-2 gap-y-[65px]'>
+          <PortfolioCard 
+            imagePath={'portfolio1.png'} 
+            title={'Service Track'} 
+            description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur iure provident mollitia ex facilis adipisci et perspiciatis error suscipit dignissimos?'} 
+            activeLink={'https://google.com'} 
+            gitLink={'https://google.com'}
+          />
+          <PortfolioCard 
+            imagePath={'portfolio1.png'} 
+            title={'Service Track'} 
+            description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur iure provident mollitia ex facilis adipisci et perspiciatis error suscipit dignissimos?'} 
+            activeLink={'https://google.com'} 
+            gitLink={'https://google.com'}
+          />
+          <PortfolioCard 
+            imagePath={'portfolio1.png'} 
+            title={'Service Track'} 
+            description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur iure provident mollitia ex facilis adipisci et perspiciatis error suscipit dignissimos?'} 
+            activeLink={'https://google.com'} 
+            gitLink={'https://google.com'}
+          />
+          <PortfolioCard 
+            imagePath={'portfolio1.png'} 
+            title={'Service Track'} 
+            description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur iure provident mollitia ex facilis adipisci et perspiciatis error suscipit dignissimos?'} 
+            activeLink={'https://google.com'} 
+            gitLink={'https://google.com'}
+          />
+        </div>
       </div>
       <div className='mt-[95px]'>
         <Social_Links/>
