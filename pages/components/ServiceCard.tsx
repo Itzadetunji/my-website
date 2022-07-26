9ceb8964c59c122892dda4457494e02a5496c9c6
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2022-07-26 12:18:27 +0100

    Chore: Completed servicecard again ✅

--- pages/components/ServiceCard.tsx
+++ pages/components/ServiceCard.tsx
@@ -9,7 +9,7 @@ interface cardProps {
 
 const ServiceCard: React.FC<cardProps> = ({iconPath, title, description}) => {
   return (
-    <div className='grey w-[300px] sm:w-[367px] lg:w-[250px] xl:w-[367px] rounded-[30px] mx-auto custom-shadow duration-500 ease-in delay-[10ms]'>
+    <div className='grey w-[300px] sm:w-[367px] lg:w-[290px] xl:w-[367px] rounded-[30px] mx-auto custom-shadow duration-500 ease-in delay-[10ms]'>
       <div className='flex flex-col'>
         <img src={`/icons/service-icons/${iconPath}`} alt="" className='w-[90px] h-[90px] items-center justify-center mx-auto mt-[47px]'/>
         <p className='text-center text-white pt-[35px] font-bold light-blue-text text-[23px]'>{title}</p>
