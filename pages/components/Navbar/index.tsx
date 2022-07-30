09749748558d4a9bd3093f8edf5fd81106c514b8
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-07-30 17:52:01 +0100

    Chore: Updated navbar.tsx

---
+++ pages/components/Navbar/index.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from 'next'
+import React from 'react'
+
+const index: NextPage = () => {
+  return (
+    <nav className='dark-blue w-full h-[109px]'>
+      <div className='flex flex-row mx-[30px]'>
+
+      </div>
+    </nav>
+  )
+}
+
+export default index
