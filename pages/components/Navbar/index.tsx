16d7458972a9f85c06b8e6ddbb68717807787a91
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-07-30 17:58:38 +0100

    Chore: Updated navbar and added linkedin logo

--- pages/components/Navbar/index.tsx
+++ pages/components/Navbar/index.tsx
@@ -4,8 +4,19 @@ import React from 'react'
 const index: NextPage = () => {
   return (
     <nav className='dark-blue w-full h-[109px]'>
-      <div className='flex flex-row mx-[30px]'>
-
+      <div className='flex flex-row mx-[30px] py-[20px] justify-between'>
+        <div>
+          <img src="/icons/logo.svg" alt="" />
+        </div>
+        <div className='flex spcae-x-[30px] text-white'>
+          <p>Home</p>
+          <p>About</p>
+          <p>Contact</p>
+        </div>
+        <div className='flex spcae-x-[30px]'>
+          <img src="/icons/logo.svg" alt="" />
+          <img src="/icons/logo.svg" alt="" />
+        </div>
       </div>
     </nav>
   )
