b424833f31ed08323f038d24b027ccbb21448110
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-07-30 18:06:39 +0100

    Chore: Updated navbar

--- pages/components/Navbar/index.tsx
+++ pages/components/Navbar/index.tsx
@@ -8,13 +8,14 @@ const index: NextPage = () => {
         <div>
           <img src="/icons/logo.svg" alt="" />
         </div>
-        <div className='flex spcae-x-[30px] text-white'>
-          <p>Home</p>
-          <p>About</p>
-          <p>Contact</p>
+        <div className='flex text-white text-[21px] my-[20px]'>
+          <p className='pr-[50px] text-[#5CBEFF]'>Home</p>
+          <p className='pr-[50px] hover:text-[#5CBEFF] duration-300'>About</p>
+          <p className='pr-[50px] hover:text-[#5CBEFF] duration-300'>Portfolio</p>
+          <p className='hover:text-[#5CBEFF] duration-300'>Contact</p>
         </div>
-        <div className='flex spcae-x-[30px]'>
-          <img src="/icons/social-icons/github.svg" alt="" />
+        <div className='flex  my-[15px]'>
+          <img src="/icons/social-icons/github.svg" className='w-[34px] h-[34px] mr-[30px]' alt="" />
           <img src="/icons/social-icons/linkedin.svg" className='w-[34px] h-[34px]' alt="" />
         </div>
       </div>
