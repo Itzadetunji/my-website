f14f8cd3cd32f999cd3942a4a67ff4979d6ca307
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-07-31 16:09:31 +0100

    Chore: Updated navbar component

--- pages/components/Navbar/index.tsx
+++ pages/components/Navbar/index.tsx
@@ -1,14 +1,15 @@
 import { NextPage } from 'next'
 import { useRouter } from "next/router"
 import Link from 'next/link'
-import React from 'react'
+import { useState } from 'react'
 import NavItem from './NavItem'
 
 const index: NextPage = () => {
   const router = useRouter().pathname;
+  const [activeHam, setActiveHam] = useState(false);
   return (
     <nav className='dark-blue w-full h-[109px] sticky top-0 z-50'>
-      <div className='flex flex-row ml-[60px] mr-[75px] py-[20px] justify-between'>
+      <div className='flex flex-row ml-[60px] lg:ml-[60px] mr-[60px] lg:mr-[75px] py-[20px] justify-between'>
         <div>
           <img src="/icons/repeated.svg" className='w-[65px] h-[65px]' alt="" />
         </div>
@@ -34,7 +35,7 @@ const index: NextPage = () => {
             </div>
           </a>
         </div>
-        <div className='flex my-[15px]'>
+        <div className='hidden lg:flex my-[15px]'>
           <Link href={'https://github.com/itzadetunji'}>
             <div className='cursor-pointer'>
               <img src="/icons/social-icons/github.svg" className='w-[34px] h-[34px] mr-[30px]' alt="" />
@@ -45,6 +46,9 @@ const index: NextPage = () => {
               <img src="/icons/social-icons/linkedin.svg" className='w-[34px] h-[34px]' alt="" />
             </div>
           </Link>
+        </div>
+        <div className='flex lg:hidden hover:bg-[pink]'>
+          <img src="/icons/hamburger.svg" className='py-[5px] px-[5px]' alt="" />
         </div>
       </div>
     </nav>
